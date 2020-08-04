6cd5c7f3982665d5afa33e8714d74866ea4d305a
Kattkovacs <kattkovacs@gmail.com>
2020-08-04 10:02:46 +0200

    optimise indices

--- optimise-indices-after-modification.sql
+++ optimise-indices-after-modification.sql
@@ -1,1 +1,6 @@
--- Please write the task's solution below
+
+SELECT last_name, first_name, COUNT(Distinct last_name) as counter
+FROM codecoolers
+WHERE last_name = first_name
+GROUP BY last_name, first_name
+ORDER BY counter;
